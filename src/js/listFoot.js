cb627ff49840d125ed3794f32655acb836d15247
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-27 17:49:12 +0200

    create file js list foot & add array object

---
+++ src/js/listFoot.js
@@ -0,0 +1,90 @@
+const dcComics = [
+    {
+    text:'Characters'
+    },
+    {
+    text:'Comics'
+    },
+    {
+    text:'Movies'
+    },
+    {
+    text:'TV'
+    },
+    {
+    text:'Games'
+    },
+    {
+    text:'Videos'
+    },
+    {
+    text:'News'
+    },
+]
+
+const dc = [
+    {
+        text:'Terms Of Use'
+    },
+    {
+        text:'Privacy policy (New)'
+    },
+    {
+        text:'Ad Choices'
+    },
+    {
+        text:'Advertising'
+    },
+    {
+        text:'Jobs'
+    },
+    {
+        text:'Subscription'
+    },
+    {
+        text:'Talent Workshops'
+    },
+    {
+        text:'CPSC Certificates'
+    },
+    {
+        text:'Ratings'
+    },
+    {
+        text:'Shop Help'
+    },
+    {
+        text:'Contact Us'
+    },
+]
+
+
+const sites = [
+    {
+        text:'DC'
+    },
+    {
+        text:'MAD Magazine'
+    },
+    {
+        text:'DC Kids'
+    },
+    {
+        text:'DC Universe'
+    },
+    {
+        text:'DC Power Visa'
+    },
+]
+
+const shop = [
+    {
+        text:'Shop DC'
+    },
+    {
+        text:'Shop DC Collectibles'
+    }
+]
+
+
+export {dcComics, dc, sites, shop};
