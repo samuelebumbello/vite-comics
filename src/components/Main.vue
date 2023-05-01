c44195b70ab0d0505ec971edbfc055d573450daa
samuelebumbello <samuelebumbello17@gmail.com>
2023-05-01 22:04:18 +0200

    [add] img main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,18 @@
 <script>
+import jsonCard from "../data/dc-comics.json"
+
 export default {
-  name: "Main"
+  name: "Main",
+  props:{
+    img: String,
+    title: String
+  },
+
+  data() {
+    return{
+      jsonCard
+    }
+}
 }
 </script>
 
@@ -16,14 +28,29 @@ export default {
 
   <main>
 
+      <div class="cards-container">
+        <div class="card" v-for="(element, index) in jsonCard" :key="index">
+          <img :src="element.thumb" alt="">
+          <span class="uppercase">{{ element.series }}</span>
+        </div>
+  
+        <div class="load-more">
+            <span>LOAD MORE</span>
+        </div>
+      </div>
+
+
   </main>
 </template>
 
 
 <style long="scss" scoped>
   main{
-    height: 150px;
+    height: 700px;
     background-color:#1C1C1C;
+    display: flex;
+    justify-content: center;
+    padding-bottom: 18px;
   }
 
   jumbotron{
@@ -34,15 +61,50 @@ export default {
   }
 
   .current-series{
-          position: absolute;
-          background-color: #0282F9;
-          padding: 10px 25px;
-          color: white;
-          font-size: 27px;
-          font-weight: 600;
-          bottom: -25px;
-          left: 18%;
-        }
+    position: absolute;
+    background-color: #0282F9;
+    padding: 10px 30px;
+    color: white;
+    font-size: 27px;
+    font-weight: 600;
+    bottom: -25px;
+    left: 16.5%;
+  }
+
+  .cards-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 70%;
+  }
+
+  .card{
+    height: 230px;
+    width: 180px;
+    color: white;
+    margin: 50px 16px;
+  }
+
+  img{
+    height: 180px;
+    width: 180px;
+    margin-bottom: 10px;
+    object-fit: cover;
+  }
+
+  .load-more{
+    background-color: #0282F9;
+    color:white;
+    font-size: 15px;
+    padding: 5px 50px;
+    margin-bottom:5px ;
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .uppercase{
+    text-transform: uppercase;
+  }
 
 
 </style>
